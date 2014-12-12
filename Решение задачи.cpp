d8b090d5b9e88ccbc60b7596f62f43e32afcf6eb
DDHunter13 <1310nero@mail.ru>
2014-12-12 17:24:52 +0300

    Update Решение задачи.cpp

--- Решение задачи.cpp
+++ Решение задачи.cpp
@@ -9,12 +9,16 @@ class Data {
 
 private:
 	std::string *item;
+	std::string *newitem;
 	int number;
 	int size = 10;
 
 public:
 		// Вывод содержимого
 	std::string get_item(const int num) {
+		if (num > size - 1) {
+			return 0;
+		}
 		return item[num];
 	}
 
@@ -22,8 +26,17 @@ public:
 	void set_item(const std::string *r_item) {
 		item[number++] = *r_item;
 		if (number == size) {
+			newitem = new std::string[size];
+			for (int i = 0; i < size ; ++i) {
+				newitem[i] = item[i];
+			}
+			delete[] item;
 			size *= 2;
-			void *realloc(void *item, size_t size);
+			item = new std::string[size];
+			for (int i = 0; i < size / 2; ++i) {
+				item[i] = newitem[i];
+			}
+			delete[] newitem;
 		}
 	}
 	
@@ -31,30 +44,35 @@ public:
 	int find_item(const std::string *f_item) {
 		for (int i = 0; i < size; ++i) {
 			if (item[i] == *f_item) {
-				return 0;
+				return 1;
 			}
 		}
-		return -1;
+		return 0;
 	}
 
 		// Удаление первого найденного предмета
 	int first_delete_item(const std::string *f_d_item) {
 		for (int i = 0; i < size; ++i) {
 			if (item[i] == *f_d_item) {
-				item[i] = "";
-				return 0;
+				for (int j = i; j < size + 1; ++j) {
+					item[j] = item[j + 1];
+				}
+				return 1;
 			}
 		}
-		return -1;
+		return 0;
 	}
 
 	// Удаление найденных предметов
 	int delete_item(const std::string *d_item) {
-		int flag = -1;
+		int flag = 0;
 		for (int i = 0; i < size; ++i) {
 			if (item[i] == *d_item) {
-				item[i] = "";
-				flag = 0;
+				for (int j = i; j < size + 1; ++j) {
+					item[j] = item[j + 1];
+				}
+				flag = 1;
+				--i;
 			}
 		}
 		return flag;
